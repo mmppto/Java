e599c825b26cf29d6131457f2db4ee84f81c9f0f
mmppto <mmppto@outlook.com>
2023-08-07 23:24:55 +0800

    集合

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Test {
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(1, 10);
+        map.put(2, 3);
+        map.put(3, 123);
+        Map.Entry<Integer, Integer> up = map.ceilingEntry(2);
+        System.out.println(up.getKey());
+    }
 }
